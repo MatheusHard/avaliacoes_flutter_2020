bd7826002702491df303cd8b39ea0d71c415a511
Matheushard <matheushard2013@gmail.com>
2020-04-02 21:54:09 -0300

    Scafold Cadastro Spinner e Textfield Nome

--- lib/strings/strings.dart
+++ lib/strings/strings.dart
@@ -1,7 +1,24 @@
 class Textos {
 
+  String titulo00 = "Selecione a Cidade";
+  String titulo01 = "Conteúdo Teórico";
+  String titulo02 = "Conteúdo Prático";
+  String titulo03 = "Instrutor";
+  String titulo04 = "Equipe de Apoio";
+  String titulo05 = "Sugestões";
+  String titulo06_Cidade = "Cidade";
+  String titulo07_Nome = "Nome";
+  String titulo08_prof = "Profissão";
+
+
+  String sub01_aplicacao = "Aplicação no processo de Trabalho:";
+  String sub02_clareza = "Clareza/Facilidades de trabalho:";
+  String sub03_carga = "Carga Horária:";
+  String sub04_conhecimento = "Cohecimento do Conteúdo:";
+  String sub05_clareza_ex = "Clareza na Exposição:";
+  String sub06_disponibilidade = "Disponibilidade para esclarecer dúvidas:";
+
   String sim1 = "Proporcionou Novos Conhecimentos";
-  String prop = "Proporcionou Novos Conhecimentos?";
   String nao1 = "Não proporcionou Novos Conhecimentos";
 
 }
--- lib/ui/cadastro_avaliacao.dart
+++ lib/ui/cadastro_avaliacao.dart
@@ -1,3 +1,4 @@
+import 'package:avaliacao_json_novo/models/Avaliacao.dart';
 import 'package:avaliacao_json_novo/models/Cidade.dart';
 import 'package:avaliacao_json_novo/strings/strings.dart';
 import 'package:flutter/cupertino.dart';
@@ -11,7 +12,11 @@ class Cadastro_Avaliacao extends StatefulWidget {
 
 class _Cadastro_AvaliacaoState extends State<Cadastro_Avaliacao> {
 
-/************SPINNER************/
+
+  TextEditingController _sugestaoController = new TextEditingController();
+  TextEditingController _nomeController = new TextEditingController();
+
+  /************SPINNER************/
 
   static List<Cidade> getCidades(){
 
@@ -39,8 +44,16 @@ class _Cadastro_AvaliacaoState extends State<Cadastro_Avaliacao> {
     for(Cidade cidade in cidades){
       items.add(
         DropdownMenuItem(
+
           value: cidade,
-          child: Text(cidade.descricao_cidade),
+          child: Center(
+            child: Text(cidade.descricao_cidade.toUpperCase(),
+                        style: TextStyle(
+                         fontWeight: FontWeight.w800,
+                         color: Colors.teal,
+                        fontSize: 15.0
+              ),),
+          ),
         )
       );
       
@@ -56,14 +69,23 @@ class _Cadastro_AvaliacaoState extends State<Cadastro_Avaliacao> {
 
 /****************************8*********************************/
 
-
-
+  int _r1 = 0;
+  int _r2 = 0;
+  int _r3 = 0;
+  int _r4 = 0;
+  int _r5 = 0;
+  int _r6 = 0;
+  int _r7 = 0;
+  int _r8 = 0;
+  int _r9 = 0;
+  int _r10 = 0;
 
   int _group01 = 0;
   int _radioSim1 = 0;
   int _radioNao1 = 0;
   double valor = 5;
   String label = "Valor Selecionado";
+
 
   @override
   Widget build(BuildContext context) {
@@ -73,76 +95,65 @@ class _Cadastro_AvaliacaoState extends State<Cadastro_Avaliacao> {
       ),
       body: SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(60),
+        padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
         child: Container(
           child: Column(
             children: <Widget>[
 
-              Text("Selecione a Cidade"),
-              SizedBox(height: 20.0,),
-              DropdownButton(
-                  value: _selectedCidade,
-                  items: _dropdownMenuItems,
-                  onChanged: onChangedDropdownItem),
-              SizedBox(height: 20.0,),
-              Text("Selecionou: ${_selectedCidade.descricao_cidade}"),
+              _textoSubtitulo(Textos().titulo00),
+
               Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(Textos().prop),
+                padding: const EdgeInsets.fromLTRB(28, 0, 28, 0),
+                child: DropdownButton(
+                    isExpanded: true,
+                    value: _selectedCidade,
+                    items: _dropdownMenuItems,
+                    onChanged: onChangedDropdownItem),
               ),
-                  RadioListTile(
-                      title: Text(Textos().sim1) ,
+              SizedBox(height: 10.0,),
+              //Text("Selecionou: ${_selectedCidade.descricao_cidade}"),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
+                child: TextField(
+                  controller: _nomeController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Nome:",
+                      hintText: "digite o nome",
+                      icon: Icon(Icons.perm_identity)
+                  ),
+                ),
+              ),
+
+
+              _textoSubtitulo(Textos().sub01_aplicacao),
+
+
+              RadioListTile(
+                      title: _textoNormal(Textos().sim1) ,
                       value: 1,
                       activeColor: Colors.greenAccent,
-                      groupValue: _group01,
+                      groupValue: _r1,
                       onChanged: (int escolha){
                         setState(() {
-                          _group01 = escolha;
+                          _r1 = escolha;
                         });
                       }
                   ),
                   RadioListTile(
-                      title: Text(Textos().nao1) ,
+                      title: _textoNormal(Textos().nao1) ,
                       value: 2,
                       activeColor: Colors.redAccent,
-                      groupValue: _group01,
+                      groupValue: _r1,
                       onChanged: (int escolha){
                         setState(() {
-                          _group01 = escolha;
+                          _r1 = escolha;
                         });
                       }
                   ),
 
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(Textos().prop),
-              ),
-              RadioListTile(
-                  title: Text(Textos().sim1) ,
-                  value: 1,
-                  activeColor: Colors.greenAccent,
-                  groupValue: _group01,
-                  onChanged: (int escolha){
-                    setState(() {
-                      _group01 = escolha;
-                    });
-                  }
-              ),
-              RadioListTile(
-                  title: Text(Textos().nao1) ,
-                  value: 2,
-                  activeColor: Colors.redAccent,
-                  groupValue: _group01,
-                  onChanged: (int escolha){
-                    setState(() {
-                      _group01 = escolha;
-                    });
-                  }
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(Textos().prop),
-              ),
+              _textoSubtitulo(Textos().sub01_aplicacao),
+
               RadioListTile(
                   title: Text(Textos().sim1) ,
                   value: 1,
@@ -166,7 +177,30 @@ class _Cadastro_AvaliacaoState extends State<Cadastro_Avaliacao> {
                   }
               ),
 
+               _textoSubtitulo(Textos().sub02_clareza),
 
+              RadioListTile(
+                  title: Text(Textos().sim1) ,
+                  value: 1,
+                  activeColor: Colors.greenAccent,
+                  groupValue: _group01,
+                  onChanged: (int escolha){
+                    setState(() {
+                      _group01 = escolha;
+                    });
+                  }
+              ),
+              RadioListTile(
+                  title: Text(Textos().nao1) ,
+                  value: 2,
+                  activeColor: Colors.redAccent,
+                  groupValue: _group01,
+                  onChanged: (int escolha){
+                    setState(() {
+                      _group01 = escolha;
+                    });
+                  }
+              ),
               RaisedButton(
                 child: Text("Aperte",
                   style: TextStyle(
@@ -176,41 +210,6 @@ class _Cadastro_AvaliacaoState extends State<Cadastro_Avaliacao> {
                 onPressed: _resultado
               ),
 
-              /********************OUTRO**************************/
-              Padding(
-                padding: const EdgeInsets.all(50.0),
-                child: Text("*******************************"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(Textos().prop),
-              ),
-              Slider(
-                  value: valor,
-                  min: 0,
-                  max: 10,
-                  label: label,
-                  divisions: 10,
-                  activeColor: Colors.green,
-                  inactiveColor: Colors.black26,
-                  onChanged: (double novoValor){
-                    setState(() {
-                      valor = novoValor;
-                      label =  novoValor.toString();
-                    //  print("Valor: "+ novoValor.toString());
-                    });
-                  }),
-              RaisedButton(
-                  child: Text("Aperte",
-                    style: TextStyle(
-                        fontSize: 20
-                    ),
-                  ),
-                  onPressed: (){
-                    print("Valor: "+ valor.toString());
-
-                  }
-              )
             ],
           ),
         ),
@@ -220,25 +219,95 @@ class _Cadastro_AvaliacaoState extends State<Cadastro_Avaliacao> {
   }
 
   void _resultado (){
-
+    Avaliacao a;
     setState(() {
-      if(_group01 == 0){
-        print("Preencha uma opção, por favor!!!");
-       }
+     a = new Avaliacao();
 
-      /*********Radios*********/
+      //Validação 02:
+      if(_r1 > 0 ){
 
-      if(_group01 == 1){
-        _radioSim1 = 1;
-        _radioNao1 = 0;
-      }if(_group01 == 2){
-        _radioNao1 = 1;
-        _radioSim1 = 0;
-      }
-    });
+      /*********Radio 01*********/
+
+        switch(_r1){
+          case 1:
+            a.radioSim_1 = 1;
+            a.radioNao_1 = 0;
+            break;
+          case 2:
+            a.radioSim_1 = 0;
+            a.radioNao_1 = 1;
+            break;
+          }
+
+      /*  switch(_r2){
+          case 1:
+            _radioSim1 = 1;
+            _radioNao1 = 0;
+            break;
+          case 2:
+            _radioNao1 = 1;
+            _radioSim1 = 0;
+            break;
+        }*/
+      //Fim validação 02:
+    }else{ print("Preencha uma opção, por favor!!!");}
+
+
+    }
+      );
+
+
+
+
     print("Resultado: " + _group01.toString()
-        + " -- Sim: " + _radioSim1.toString()
-        + " -- Não:"  +_radioNao1.toString());
+        + " -- Sim: " + a.radioSim_1.toString()
+        + " -- Não:"  +a.radioNao_1.toString());
 
+  }
+
+
+
+  Padding _textotitulo(String valor){
+
+    Padding texto = Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:  Text(valor,
+          style: TextStyle(
+              color: Colors.black26,
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold
+          ),
+        )
+    );
+    return texto;
+  }
+  Padding _textoSubtitulo(String valor){
+
+    Padding texto = Padding(
+        padding: const EdgeInsets.all(8.0),
+        child:  Text(valor,
+                    style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold
+    ),
+    )
+    );
+    return texto;
+  }
+
+  Padding _textoNormal(String valor){
+
+    Padding texto = Padding(
+        padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+        child:  Text(valor,
+          style: TextStyle(
+              color: Colors.black26,
+              fontSize: 12.0,
+              fontWeight: FontWeight.bold
+          ),
+        )
+    );
+    return texto;
   }
 }
