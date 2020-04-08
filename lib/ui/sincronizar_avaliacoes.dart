27acfab4c500131276197e8866edae64468013f3
Matheushard <matheushard2013@gmail.com>
2020-04-08 14:39:38 -0300

    Button Sincronizar

--- lib/ui/sincronizar_avaliacoes.dart
+++ lib/ui/sincronizar_avaliacoes.dart
@@ -1,6 +1,7 @@
 import 'package:avaliacao_json_novo/apis/cidade_api.dart';
 import 'package:avaliacao_json_novo/apis/uf_api.dart';
 import 'package:avaliacao_json_novo/ui/avaliacoes_db.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ModuloSincronismo {
@@ -13,34 +14,34 @@ return Container(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
+
                   RaisedButton(
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(18.0),
-                        side: BorderSide(color: Colors.red)),
-
                     onPressed: _sincronizar,
-
-                    textColor: Colors.white,
-                    padding: const EdgeInsets.all(0.0),
-                    child: Container(
-
-                      decoration: const BoxDecoration(
-                        gradient: RadialGradient(
-                          colors: <Color>[
-                            Color(0xFF0D47A1),
-                            Color(0xFF1976D2),
-                            Color(0xFF42A5F5),
-                          ],
-                        ),
-                      ),
-                      padding: const EdgeInsets.all(10.0),
-
-                      child: const Text(
-                          'Gradient Button',
-                          style: TextStyle(fontSize: 20)
-                      ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80.0),
                     ),
+                    padding: EdgeInsets.all(0.0),
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [ Color(0xFF0D47A1),
+                                                           Color(0xFF1976D2),
+                                                           Color(0xFF42A5F5),],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight
+                      ),
+                      borderRadius: BorderRadius.circular(30.0)
+                    ),
+                      child: Container(
+                        constraints: BoxConstraints( maxWidth: 300.0,minHeight: 50.0),
+                        alignment: Alignment.center,
+                        child: Text("Sincronizar",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white
+                        ),),
+                      ),
                   ),
+                )
           ],
         ),
 
